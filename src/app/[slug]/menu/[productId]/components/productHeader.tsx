1ce4fe97b61218fdbae6accbbfd996056e359067
Vicenzo <marcosvicenzo8@gmail.com>
2025-02-22 16:33:30 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/components/productHeader.tsx
+++ src/app/[slug]/menu/[productId]/components/productHeader.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
@@ -7,40 +7,42 @@ import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
-interface ProductHeaderProps{
-    product: Pick<Product, 'name' | 'imageUrl'>;
+interface ProductHeaderProps {
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
-const ProductHeader = ({product}: ProductHeaderProps) => {
-    const router = useRouter();
-    const handleBackClick = () => router.back();
-    return ( 
-        <div className="relative w-full h-[300px]">
-        <Button
-            variant="secondary"
-            size="icon"
-            className="absolute left-4 top-4 z-50 rounded-full"
-            onClick={handleBackClick}
-        >
-          <ChevronLeftIcon />
-        </Button>
-        
-        <Image 
-            src={product.imageUrl}
-            alt={product.name}
-            fill
-            className="objects-contain"
-        />
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
 
-        <Button
+  return (
+    <div className="relative w-full h-[300px] flex flex-col items-center justify-center">
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain object-center"
+      />
+
+      <Button
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
       >
         <ScrollTextIcon />
       </Button>
-      </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default ProductHeader;
